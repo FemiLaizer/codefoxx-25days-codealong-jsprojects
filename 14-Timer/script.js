8a495a16f64f02d5dced9541e4b3e402d9323316
Femi Laizer <femilaizer@gmail.com>
2022-08-15 23:42:44 +0100

    added timer projects

--- 14-Timer/script.js
+++ 14-Timer/script.js
@@ -1,14 +1,33 @@
 const span = document.querySelectorAll("span");
 const timeBox = document.querySelector('.time-box');
+let minuteDouble = document.getElementById('minute-double');
+let minuteSingle = document.getElementById('minute-single');
+let secondDouble = document.getElementById('second-double');
+let secondSingle = document.getElementById('second-single');
+let miliDouble = document.getElementById('mili-double');
+let miliSingle = document.getElementById('mili-single');
+let count = 0;
 
 timeBox.addEventListener('click', start);
 
 timeBox.addEventListener('dblclick', stop);
 
 function start() {
-    console.log('start');
+    if (minuteDouble.textContent < 9) {
+        minuteSingle.textContent++;
+    } if (minuteSingle.textContent < 9) {
+        secondDouble.textContent++;
+    } if (secondDouble.textContent < 9) {
+        secondSingle.textContent++;
+    } if (secondSingle.textContent < 9) {
+        miliDouble.textContent++;
+    } if (miliDouble.textContent < 9) {
+        count++
+        miliSingle.textContent = count;
+    }
 }
 
 function stop() {
     console.log('stop');
 }
+
